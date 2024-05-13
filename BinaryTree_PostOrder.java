e6657afefaf2b6f5f7ca7e3d1165802a5eb88774
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2024-05-13 16:56:20 +0530

    Add files via upload

---
+++ BinaryTree_PostOrder.java
@@ -0,0 +1,47 @@
+package DSA;
+
+// Code of Postorder traversel in Binary tree
+class BinaryTree_PostOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class binary_Tree_Postorder {
+        static int idx = -1;
+
+        public static Node BuildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        binary_Tree_Postorder tree = new binary_Tree_Postorder();
+        Node root = tree.BuildTree(nodes);
+        postorder(root);
+    }
+}

b3613298a2ebf5f303125eb0132fdfd995a5083b
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2024-05-13 16:56:45 +0530

    Add files via upload

---
+++ BinaryTree_InOrder.java
@@ -0,0 +1,49 @@
+package DSA;
+
+// Code of Inorder traversel in Binary tree
+class BinaryTree_InOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class binary_Tree_Inorder {
+        static int idx = -1;
+
+        public static Node BuildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void Inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Inorder(root.left);
+        System.out.print(root.data + " ");
+        Inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        binary_Tree_Inorder tree = new binary_Tree_Inorder();
+        Node root = tree.BuildTree(nodes);
+        Inorder(root);
+    }
+
+}

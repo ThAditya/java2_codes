81ceb393aab398d0f7d2b5beddadb8fa7f7ee28b
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2024-05-13 16:57:05 +0530

    Add files via upload

---
+++ BinaryTree_PreOrder.java
@@ -0,0 +1,48 @@
+package DSA;
+
+// Code of Preorder traversel in Binary tree
+class BinaryTree_PreOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class binary_Tree_Preoder {
+        static int idx = -1;
+
+        public static Node BuildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        binary_Tree_Preoder tree = new binary_Tree_Preoder();
+        Node root = tree.BuildTree(nodes);
+        preorder(root);
+    }
+}
